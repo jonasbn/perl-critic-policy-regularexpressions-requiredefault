4ad652b6b8040611154dbce7499ef4d726387d68
jonasbn <jonasbn@gmail.com>
2018-09-16 22:21:38 +0200

    More documentation added

--- lib/Perl/Critic/Policy/RegularExpressions/RequireDefault.pm
+++ lib/Perl/Critic/Policy/RegularExpressions/RequireDefault.pm
@@ -18,7 +18,7 @@ Readonly::Scalar my $EXPL => q{Use regular expression "/a" or "/aa" flag};
 
 #-----------------------------------------------------------------------------
 
-sub default_severity     { return $SEVERITY_MEDIUM           }
+sub default_severity     { return $SEVERITY_MEDIUM }
 sub default_themes       { return qw< security > }
 sub applies_to           {
     return qw<
@@ -51,14 +51,70 @@ __END__
 
 Perl::Critic::Policy::RegularExpressions::RequireDefault - Always use the C</a> or C</aa> modifier with regular expressions.
 
+=head1 AFFILIATION
+
+This policy is affiliated with the Perl::Critic::JONASBN policy bundle.
+
+=head1 DESCRIPTION
+
+The C</a> modifier's purpose is to allow code that is to work mostly on ASCII data to not have to concern itself with Unicode.
+
 This policy is inspired by and many implementation details was lifted from L<Perl::Critic::Policy::RegularExpressions::RequireExtendedFormatting|https://metacpan.org/pod/Perl::Critic::Policy::RegularExpressions::RequireExtendedFormatting>
+
+=head1 CONFIGURATION
+
+This policy is not configurable at this time. Please see the TODO L</section>.
+
+=head1 NOTES
+
+=head1 TODO
+
+=over
+
+=item * Please see L<the issue listing on GitHub|https://github.com/jonasbn/perl-critic-policy-regularexpressions-requiredefault/issues|>
+
+=back
 
 =head1 SEE ALSO
 
 =over
 
-=item * https://perldoc.perl.org/perlre.html
+=item * L<Perl regular expression documentation: perlre|https://perldoc.perl.org/perlre.html>
+
+=item * L<Perl::Critic::Policy::RegularExpressions::RequireExtendedFormatting|https://metacpan.org/pod/Perl::Critic::Policy::RegularExpressions::RequireExtendedFormatting>
 
 =back
+
+=head1 MOTIVATION
+
+The motivation for this Perl::Critic policy came from a L<tweet|https://mobile.twitter.com/jmaslak/status/1008896883169751040>| by L<@joel|https://mobile.twitter.com/jmaslak>
+
+    | Perl folk: Looking for a PR challenge task? Check for \d in regexes
+    | that really should be [0-9] or should have the /a regex modifier.
+    | Perl is multinational by default! #TPCiSLC
+
+=head1 AUTHOR
+
+=over
+
+=item * jonasbn <jonasbn@cpan.org>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=over
+
+=item * L<Joelle Maslak (@joel)|https://twitter.com/jmaslak> / L<JMASLAK|https://metacpan.org/author/JMASLAK>
+
+=back
+
+=head1 COPYRIGHT
+
+Perl::Critic::Policy::RegularExpressions::RequireDefault is (C) by jonasbn 2018
+
+Perl::Critic::Policy::RegularExpressions::RequireDefault is released under the Artistic License 2.0
+
+Please see the LICENSE file included with the distribution of this module
 
 =cut
